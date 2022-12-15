a7a3b8f835f22370d70bf04debbe83986a2ad722
fernandiego <fernandodiego.sn@gmail.com>
2022-12-14 22:17:03 -0300

    Desafio 003 finalizado, removendo itens do ArrayList e refazendo a pergunta de tarefas concluídas

--- desafio003/ListaDeTarefas.java
+++ desafio003/ListaDeTarefas.java
@@ -1,6 +1,8 @@
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
+
 
 public class ListaDeTarefas {
     public static void main(String[] args) {
@@ -10,13 +12,31 @@ public class ListaDeTarefas {
 //            System.out.println(nome);
         }
 
-        List <String> lista =  Arrays.asList(listaPronta.split(","));
+        List<String> lista = new ArrayList(Arrays.asList(listaPronta.split(",")));
 //        String values = "book2s.com ,asdfa asdf ,asdf asdf, asdf asdf ";
 //        System.out.println(lista);
 
-        for (int i = 0; i < lista.size(); i++) {
+//        System.out.println(lista);
+//        lista.remove(0);
+        while (lista.size() > 0) {
 
-            System.out.println(i+1+". "+ lista.get(i));
+            for (int i = 0; i < lista.size(); i++) {
+
+                System.out.println(i + 1 + "." + lista.get(i).trim());
+            }
+            System.out.println("Qual tarefa você concluiu?");
+            Scanner tarefaConcluida = new Scanner(System.in);  // Create a Scanner object
+
+            int removerIndex = tarefaConcluida.nextInt();
+            lista.remove(removerIndex - 1);
+
         }
+//        System.out.println(lista);
+
+//        for (int i = 0; i < lista.size(); i++) {
+//
+//            System.out.println(i + 1 + "." + lista.get(i).trim());
+//        }
+
     }
 }
