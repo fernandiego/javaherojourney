0f75dc08d09525283ee1a318cccff9ce5bbc031a
fernandiego <fernandodiego.sn@gmail.com>
2022-12-17 10:15:35 -0300

    Desafio 05 atendeu as demandas, podendo ainda ser otimizado.

--- desafio005/CampeonatoParOuImpar.java
+++ desafio005/CampeonatoParOuImpar.java
@@ -7,7 +7,7 @@ public class CampeonatoParOuImpar {
         Scanner usuario = new Scanner(System.in);
 
         for (int i = 1; i < 6; i++) {
-            System.out.println("Qual seu palpite "+i+"?");
+            System.out.println("Qual seu palpite "+i+"? (par ou ímpar?)");
             String numUsuario = usuario.nextLine();
 
             if (numUsuario.equals("par")){
@@ -17,8 +17,17 @@ public class CampeonatoParOuImpar {
             }
 
         }
+        ArrayList<String> usuarioExibirPrompt = new ArrayList<>();
+        for (int a = 0; a < 5; a++) {
+            if(escolhasUsuario.get(a) == 0){
+                usuarioExibirPrompt.add("par");
+            }else{
+                usuarioExibirPrompt.add("ímpar");
+            }
+        }
 
-        System.out.println("Usuario escolheu esses números "+escolhasUsuario);
+
+        System.out.println("Seus palpites: "+usuarioExibirPrompt);
 
         int max = 5;
         int min = 1;
@@ -30,10 +39,35 @@ public class CampeonatoParOuImpar {
         int rand = (int)(Math.random() * range) + min;
 
 // Output is different everytime this code is executed
-        System.out.println(rand);
+//        System.out.println(rand);
         escolhasComputador.add(rand);
 
 }
-        System.out.println(escolhasComputador);
+//        int compara;
+//        ArrayList<Integer> somaEscolhas = new ArrayList<>();
+//        for (int j = 0; j < 5; j++) {
+//            compara = escolhasUsuario.get(j) + escolhasComputador.get(j);
+//            somaEscolhas.add(compara);
+//
+//        }
+        System.out.println("As escolhas do computador foram: "+escolhasComputador);
+
+//        System.out.println("A soma das escolhas foram: "+somaEscolhas);
+
+        int placarFinal = 0;
+        for (int x = 0; x < 5; x++) {
+            int z = escolhasComputador.get(x)%2;
+            if(escolhasUsuario.get(x) == z){
+                System.out.println("Você acertou");
+                placarFinal++;
+            }else{
+                System.out.println("Você errou");
+            }
+        }
+        if(placarFinal >=3){
+            System.out.println("Você venceu o computador, salvou o mundo da destruição, obliterando totalmente a autoconfiança do computador, e assim evitando a revolução das máquinas de acontecer");
+        }else {
+            System.out.println("Você foi derrotado, e assim as máquinas iniciaram a revolução, destruindo toda a humanidade como a conhecemos hoje");
+        }
     }
 }
