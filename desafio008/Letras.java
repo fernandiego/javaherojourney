5ce9fbc3a139572daac7062107f0e3497423903b
Leonardo Silveira <sombriks@gmail.com>
2022-12-21 20:03:17 -0300

    teste

--- desafio008/Letras.java
+++ desafio008/Letras.java
@@ -1,11 +1,18 @@
+import java.io.FileInputStream;
+import java.util.Scanner;
 
 public class Letras {
 
-  public static void main(String ...args) {
+  public static void main(String ...args) throws Exception {
 
     String arquivo = args[0];
     int letras = 0;
 
-    System.out.printf("O arquivo %s tem &d letras\n", arquivo, letras);
+    Scanner input  = new Scanner(new FileInputStream(arquivo));
+    String texto = input.useDelimiter("\\A").next();
+    texto = texto.replaceAll("\\d","").replaceAll("\\W","");
+    letras = texto.length();
+
+    System.out.printf("O arquivo %s tem %d letras\n", arquivo, letras);
   }
 }
