1878061e3b82290271ff56dcbad4f657d143c5cd
Leonardo Silveira <sombriks@gmail.com>
2022-12-21 19:52:47 -0300

    novo desafio

---
+++ desafio008/Letras.java
@@ -0,0 +1,11 @@
+
+public class Letras {
+
+  public static void main(String ...args) {
+
+    String arquivo = args[0];
+    int letras = 0;
+
+    System.out.printf("O arquivo %s tem &d letras\n", arquivo, letras);
+  }
+}
