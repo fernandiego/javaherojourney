217e02186187acc3eef7eb60b6fbb32509b8a7b6
sombriks <sombriks@gmail.com>
2023-02-06 21:20:32 -0300

    novo desafio

--- desafio014/README.md
+++ desafio014/README.md
@@ -1,6 +1,6 @@
 # Desafion 014
 
-Como buscar, adicionar e remover elementos em listas, arrays e mapas
+Como buscar, adicionar e remover elementos em listas e mapas
 
 ## O que é
 
@@ -11,6 +11,8 @@ Vários snippets incompletos pra fazer uma determinada operação. Cada exercíc
 Ao rodar o programa várias mensagens devem surgir e elas devem ser corretas, exemplo
 
 ```bash
-Na lista [1,3,5,7,9] tem 5 números ímpares
-No array [3,11,5,8] o maior valor é 11
+[1,3,5,7,9] tem 5 números ímpares
+[3,11,5,8] o maior valor é 11
+[4,5,1,1,2,3] a soma dos elementos é 16
+"bodejo" tem 1 b, 1 d, 1 e, 1 j, 2 o
 ```
---
+++ desafio014/Simula.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Simula {
+
+    public static void main(String ...args) {
+        Simula sim = new Simula();
+        sim.contaImpares();
+        sim.somaNumeros();
+        sim.encontraMaior();
+        sim.contaLetras();
+    }
+
+    private String palavra = "inconstitucionalissimamente";
+
+    private Map<String,Integer> letras = new HashMap<>();
+
+    private List<Integer> numeros = Arrays.asList(1,1,2,3,8,12,9,0,1,5,3,2,7);
+
+    private void contaImpares() {
+    }
+
+    private void contaLetras() {
+    }
+
+    private void removeImpares() {
+    }
+
+    private void encontraMaior() {
+    }
+
+    private void somaNumeros() {
+    }
+
+}
